11c9f16bddbec3f6fd1beb52e631fa4fcd3abf09
Cristiano Faustino <cristiadu@gmail.com>
2023-08-08 03:54:46 +0000

    bundle install for all blogs

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "======== Building Main blog =========="
+bundle install
 bundle exec jekyll build
 echo "======================================"
 echo "======== Building Sub-Blogs =========="
@@ -8,6 +9,7 @@ home_dir=$(pwd)
 for d in blogs/*/ ; do
     echo "=> Building Blog: '$d'"
     pushd $home_dir/$d
+    bundle install
     bundle exec jekyll build --destination $home_dir/_site/${PWD##*/}
 done
 
