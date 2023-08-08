a568f4fec7a05e2e5e1c24efda104561bb8e0246
Cristiano Faustino <cristiadu@gmail.com>
2023-08-08 03:52:02 +0000

    remove custom base url

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "======== Building Main blog =========="
-bundle exec jekyll build --baseurl $BASE_URL
+bundle exec jekyll build
 echo "======================================"
 echo "======== Building Sub-Blogs =========="
 home_dir=$(pwd)
